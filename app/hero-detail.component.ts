5a78e4677f96c1abc2eb18de3379a027d04776a3
Kerem Özben <keremozben@gmail.com>
2016-09-07 14:29:48 +0300

    first commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,25 @@
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { HeroService } from './hero.service';
+
+import { Hero } from './hero';
+@Component({
+  selector: 'my-hero-detail',
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+export class HeroDetailComponent {
+  @Input() hero: Hero;
+}
